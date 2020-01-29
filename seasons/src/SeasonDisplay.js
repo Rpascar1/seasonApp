2c03f76db0ba498a156d7202fdae04e0fa5d3241
Rich <rpavesi84@gmail.com>
2020-01-28 22:11:29 -0500

    added animation and css for seasons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,40 @@
+import './SeasonDisplay.css'
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+      text:"Let's hit the beach!",
+      iconName:'sun'
+    },
+    winter: {
+      text:'Burr, it is chilly',
+      iconName:'snowflake'
+    },
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>
 }
+const getSeason = (lat, month)=>{
+  if (month > 2 && month < 9){
+    return lat > 0 ? 'summer': 'winter'
+  } else {
+    return lat > 0 ? 'winter':'summer'
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const {text, iconName} = seasonConfig[season]
+  // const text = season === 'winter' ? seasonConfig.winter.text : seasonConfig.summer.text
+  // const icon = season === 'winter' ? seasonConfig.winter.iconName : seasonConfig.summer.iconName
+  return(
+    <div>
+    <div className= {`season-display ${season}`}>
+    <i className={`icon-left spinning huge ${iconName} icon`}/>
+      <h1>{text}</h1>
+      <i className={`icon-right spinning huge ${iconName} icon`}/>
+      </div>
+    </div>
+    )
+  }
 
 
 export default SeasonDisplay
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
-  constructor (props){
-    super(props);
+  // constructor (props){
+  //   super(props);
+  //
+  //   this.state = { lat: null, errorMessage: ''}
+  //
+  // }
 
-    this.state = { lat: null, errorMessage: ''}
 
-  }
+
+
+  state = { lat: null, errorMessage: ''}
+
 
   componentDidMount(){
 
@@ -22,11 +29,19 @@ class App extends React.Component{
         return <div> Error - location request denied. </div>
         }
       if (!this.state.errorMessage && this.state.lat){
-        return <div> Latitude: {this.state.lat}</div>
-        }
+        // return <div> Latitude: {this.state.lat}</div>
+        return <SeasonDisplay lat={this.state.lat}/>
+      } else {
 
-        return <div> Loading </div>
+        return (
 
+        <div class="ui segment">
+          <div class="ui active inverted dimmer">
+            <div class="ui text loader">Loading</div>
+          </div>
+        </div>
+      )
+    }
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
